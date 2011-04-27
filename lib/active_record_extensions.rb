531daa72c8b90ecde68bd2cb5916f03d1adaa03b
Olexiy Zamkoviy <olexiy.z@gmail.com>
2011-04-27 18:10:15 +0300

    Fixed regular expressions, added some tests

--- lib/active_record_extensions.rb
+++ lib/active_record_extensions.rb
@@ -7,12 +7,17 @@ require 'active_record'
 require 'active_record_extensions/has_attached_file'
 
 ActiveRecord::Base.class_eval do
-  ALNUM_RE = /\A[а-яА-Яa-zA-Z0-9][-а-яА-Яa-zA-Z0-9\s]+\Z/u
-  ALNUM_G_RE = /^[-#{"\xC3\x80-\xC3\x96\xC3\x98-\xC3\xB6\xC3\xB8-\xE1\xBF\xBE"}a-zA-Z0-9 ]+$/u
-  ALNUM_G_EXTENDED_RE = /^[-#{"\xC3\x80-\xC3\x96\xC3\x98-\xC3\xB6\xC3\xB8-\xE1\xBF\xBE"}a-zA-Z0-9 '`’]+$/u
-  IDENTIFIER_G_RE = /^[a-zA-Z][-#{"\xC3\x80-\xC3\x96\xC3\x98-\xC3\xB6\xC3\xB8-\xE1\xBF\xBE"}a-zA-Z0-9]+$/u
-  IDENTIFIER_LIST_G_RE = /^([a-zA-Z][-#{"\xC3\x80-\xC3\x96\xC3\x98-\xC3\xB6\xC3\xB8-\xE1\xBF\xBE"}a-zA-Z0-9]+,)*[a-zA-Z][-#{"\xC3\x80-\xC3\x96\xC3\x98-\xC3\xB6\xC3\xB8-\xE1\xBF\xBE"}a-zA-Z0-9]+$/u
-  URL_RE = /^(?#Proto)(https?\:\/\/)?(?#Subdomains)([-#{"\xC3\x80-\xC3\x96\xC3\x98-\xC3\xB6\xC3\xB8-\xE1\xBF\xBE"}\w]+\.)+(?#TopLevelDomain)[#{"\xC3\x80-\xC3\x96\xC3\x98-\xC3\xB6\xC3\xB8-\xE1\xBF\xBE"}A-Za-z]{2,}(?#TheRest)\/?.*$/u
+  ALNUM_RE = /\A[a-zA-Z0-9][-a-zA-Z0-9 ]+\z/u
+  IDENTIFIER_RE = /\A[a-zA-Z][a-zA-Z0-9]*\z/u
+
+  # TODO: add tests
+  ALNUM_G_RE = /\A[-#{"\xC3\x80-\xC3\x96\xC3\x98-\xC3\xB6\xC3\xB8-\xE1\xBF\xBE"}a-zA-Z0-9 ]+\z/u
+  ALNUM_G_EXTENDED_RE = /\A[-#{"\xC3\x80-\xC3\x96\xC3\x98-\xC3\xB6\xC3\xB8-\xE1\xBF\xBE"}a-zA-Z0-9 '`’]+\z/u
+  IDENTIFIER_G_RE = /\A[a-zA-Z][-#{"\xC3\x80-\xC3\x96\xC3\x98-\xC3\xB6\xC3\xB8-\xE1\xBF\xBE"}a-zA-Z0-9]+\z/u
+
+  IDENTIFIER_LIST_RE = /\A([a-zA-Z][a-zA-Z0-9]{,99})(,[a-zA-Z][a-zA-Z0-9]{,99}){,9}\z/
+  IDENTIFIER_LIST_G_RE = /\A([a-zA-Z][-#{"\xC3\x80-\xC3\x96\xC3\x98-\xC3\xB6\xC3\xB8-\xE1\xBF\xBE"}a-zA-Z0-9]+,)*[a-zA-Z][-#{"\xC3\x80-\xC3\x96\xC3\x98-\xC3\xB6\xC3\xB8-\xE1\xBF\xBE"}a-zA-Z0-9]+\z/u
+  URL_RE = /\A(?#Proto)(https?\:\/\/)?(?#Subdomains)([-#{"\xC3\x80-\xC3\x96\xC3\x98-\xC3\xB6\xC3\xB8-\xE1\xBF\xBE"}\w]+\.)+(?#TopLevelDomain)[#{"\xC3\x80-\xC3\x96\xC3\x98-\xC3\xB6\xC3\xB8-\xE1\xBF\xBE"}A-Za-z]{2,}(?#TheRest)\/?[^\r\n]*\z/u
 
   def self.validates_as_alnum(field)
     validates_format_of field, :with => ALNUM_RE, :message => "should contain at least two symbols and begin with a letter", :if => lambda { |m| m.errors.on(field).nil? }
@@ -35,6 +40,10 @@ ActiveRecord::Base.class_eval do
 
   def self.validates_as_identifier_list_g(field)
     validates_format_of field, :with => IDENTIFIER_LIST_G_RE, :message => "is invalid", :if => lambda { |m| m.errors.on(field).nil? }
+  end
+
+  def self.validates_as_identifier_list(field)
+    validates_format_of field, :with => IDENTIFIER_LIST_RE, :message => "is invalid", :if => lambda { |m| m.errors.on(field).nil? }
   end
 
   def self.default_value_for(key, val)
--- spec/active_record_extensions_spec.rb
+++ spec/active_record_extensions_spec.rb
@@ -13,14 +13,171 @@ describe "active_record_extensions" do
   end
 
   context "URL_RE" do
-    it { "http://test.com/".should match(ActiveRecord::Base.const_get('URL_RE')) }
-    it { "https://test.com/".should match(ActiveRecord::Base.const_get('URL_RE')) }
-    it { "http://subdomain.test.com/".should match(ActiveRecord::Base.const_get('URL_RE')) }
-    it { "http://wrong domain.test.com/".should_not match(ActiveRecord::Base.const_get('URL_RE')) }
-    it { "http://wrongdomain!.test.com/".should_not match(ActiveRecord::Base.const_get('URL_RE')) }
-    it { "http://wrongdomain@.test.com/".should_not match(ActiveRecord::Base.const_get('URL_RE')) }
-    it { "http://wrongdomain#.test.com/".should_not match(ActiveRecord::Base.const_get('URL_RE')) }
-    it { "http://test/".should_not match(ActiveRecord::Base.const_get('URL_RE')) }
+    context "valid cases" do
+      it { "http://test.com/".should match(ActiveRecord::Base.const_get('URL_RE')) }
+      it { "https://test.com/".should match(ActiveRecord::Base.const_get('URL_RE')) }
+      it { "http://subdomain.test.com/".should match(ActiveRecord::Base.const_get('URL_RE')) }
+      it { "http://test.com/@!@#!@!@$".should match(ActiveRecord::Base.const_get('URL_RE')) }
+    end
+
+    context "invalid cases" do
+      it { "http://wrong domain.test.com/".should_not match(ActiveRecord::Base.const_get('URL_RE')) }
+      it { "http://wrongdomain!.test.com/".should_not match(ActiveRecord::Base.const_get('URL_RE')) }
+      it { "http://wrongdomain@.test.com/".should_not match(ActiveRecord::Base.const_get('URL_RE')) }
+      it { "http://wrongdomain#.test.com/".should_not match(ActiveRecord::Base.const_get('URL_RE')) }
+      it { "http://test/".should_not match(ActiveRecord::Base.const_get('URL_RE')) }
+
+      it "ends with newline" do
+        "http://test.com/\n".should_not match(ActiveRecord::Base.const_get('URL_RE'))
+      end
+
+      it "ends with caret return" do
+        "http://test.com/\r".should_not match(ActiveRecord::Base.const_get('URL_RE'))
+      end
+
+      it "begins with newline" do
+        "\nhttp://test.com/".should_not match(ActiveRecord::Base.const_get('URL_RE'))
+      end
+
+      it "begins with caret return" do
+        "\rhttp://test.com/".should_not match(ActiveRecord::Base.const_get('URL_RE'))
+      end
+
+      it "contains newline" do
+        "http://tes\nt.com/".should_not match(ActiveRecord::Base.const_get('URL_RE'))
+      end
+
+      it "contains caret return" do
+        "http://tes\rt.com/".should_not match(ActiveRecord::Base.const_get('URL_RE'))
+      end
+    end
+  end
+
+  context "ALNUM_RE" do
+    context "valid cases" do
+      it { "Alnum".should match(ActiveRecord::Base.const_get('ALNUM_RE')) }
+      it { "Alnum123".should match(ActiveRecord::Base.const_get('ALNUM_RE')) }
+      it { "Alnum123 ".should match(ActiveRecord::Base.const_get('ALNUM_RE')) }
+      it { "123Alnum".should match(ActiveRecord::Base.const_get('ALNUM_RE')) }
+    end
+
+    context "invalid cases" do
+      it "begins with space" do
+        " Alnum123".should_not match(ActiveRecord::Base.const_get('ALNUM_RE')) 
+      end
+
+      it { "Alnum#".should_not match(ActiveRecord::Base.const_get('ALNUM_RE')) }
+      it { "Alnum$".should_not match(ActiveRecord::Base.const_get('ALNUM_RE')) }
+
+      it "ends with newline" do
+        "Alnum\n".should_not match(ActiveRecord::Base.const_get('ALNUM_RE'))
+      end
+
+      it "ends with caret return" do
+        "Alnum\r".should_not match(ActiveRecord::Base.const_get('ALNUM_RE')) 
+      end
+
+      it "contains newline" do
+        "Alnum\nxx".should_not match(ActiveRecord::Base.const_get('ALNUM_RE')) 
+      end
+
+      it "contains caret return" do
+        "Alnum\ryy".should_not match(ActiveRecord::Base.const_get('ALNUM_RE')) 
+      end
+    end
+  end
+
+  context "IDENTIFIER_RE" do
+    context "valid cases" do
+      it { "Identifer".should match(ActiveRecord::Base.const_get('IDENTIFIER_RE')) }
+      it { "Identifier123".should match(ActiveRecord::Base.const_get('IDENTIFIER_RE')) }
+    end
+
+    context "invalid cases" do
+      it "begins with space" do 
+        " Identifier123".should_not match(ActiveRecord::Base.const_get('IDENTIFIER_RE')) 
+      end
+
+      it "ends with space" do
+        "Identifier123 ".should_not match(ActiveRecord::Base.const_get('IDENTIFIER_RE')) 
+      end
+
+      it "ends with newline" do
+        "Identifier123\n".should_not match(ActiveRecord::Base.const_get('IDENTIFIER_RE')) 
+      end
+
+      it "ends with caret return" do 
+        "Identifier123\r".should_not match(ActiveRecord::Base.const_get('IDENTIFIER_RE')) 
+      end
+
+      it "contains newline" do 
+        "Identifier123\nxx".should_not match(ActiveRecord::Base.const_get('IDENTIFIER_RE')) 
+      end
+
+      it "contains caret return" do 
+        "Identifier123\ryy".should_not match(ActiveRecord::Base.const_get('IDENTIFIER_RE')) 
+      end
+
+      it "begins with number" do 
+        "123Identifier".should_not match(ActiveRecord::Base.const_get('IDENTIFIER_RE')) 
+      end
+    end
+  end
+
+  context "IDENTIFIER_LIST_RE" do
+    context "valid cases" do
+      it { ("a" * 100).should match(ActiveRecord::Base.const_get('IDENTIFIER_LIST_RE')) }
+      it { (("a" * 100) + ',' + ("b" * 100)).should match(ActiveRecord::Base.const_get('IDENTIFIER_LIST_RE')) }
+      it { (["a" * 100] * 10).join(',').should match(ActiveRecord::Base.const_get('IDENTIFIER_LIST_RE')) }
+    end
+
+    context "invalid cases" do
+      it { ("a" * 101).should_not match(ActiveRecord::Base.const_get('IDENTIFIER_LIST_RE')) }
+
+      context "single item" do
+        it "ends with newline" do
+          ("a" * 99 + "\n").should_not match(ActiveRecord::Base.const_get('IDENTIFIER_LIST_RE'))
+        end
+
+        it "ends with caret return" do
+          ("a" * 99 + "\r").should_not match(ActiveRecord::Base.const_get('IDENTIFIER_LIST_RE'))
+        end
+
+        it "contains newline" do
+          ("a" * 50 + "\n" + "b" * 49).should_not match(ActiveRecord::Base.const_get('IDENTIFIER_LIST_RE'))
+        end
+
+        it "contains newline" do
+          ("a" * 50 + "\n" + "b" * 49).should_not match(ActiveRecord::Base.const_get('IDENTIFIER_LIST_RE'))
+        end
+      end
+
+      context "multiple items" do
+        it "last element contains newline" do
+          (("a" * 100) + ',' + ("b" * 99) + "\n").should_not match(ActiveRecord::Base.const_get('IDENTIFIER_LIST_RE'))
+        end
+
+        it "last element contains caret return" do
+          (("a" * 100) + ',' + ("b" * 99) + "\r").should_not match(ActiveRecord::Base.const_get('IDENTIFIER_LIST_RE'))
+        end
+
+        it "first element contains newline" do
+          (("a" * 99) + "\n" + ',' + ("b" * 100)).should_not match(ActiveRecord::Base.const_get('IDENTIFIER_LIST_RE'))
+        end
+
+        it "first element contains caret return" do
+          (("a" * 99) + "\r" + ',' + ("b" * 100)).should_not match(ActiveRecord::Base.const_get('IDENTIFIER_LIST_RE'))
+        end
+
+        it "newline after all elements" do
+          ((["a" * 100] * 10).join(',') + "\n").should_not match(ActiveRecord::Base.const_get('IDENTIFIER_LIST_RE'))
+        end
+
+        it { (("a" * 100) + ',' + ("b" * 101)).should_not match(ActiveRecord::Base.const_get('IDENTIFIER_LIST_RE')) }
+        it { (["a" * 100] * 11).join(',').should_not match(ActiveRecord::Base.const_get('IDENTIFIER_LIST_RE')) }
+
+      end
+    end
   end
 end
 
